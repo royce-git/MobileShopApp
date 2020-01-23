f67562c9197d5025a28c6a4153a3b2af2563e92d
royce-git <54796100+royce-git@users.noreply.github.com>
2020-01-23 23:08:57 +0100

    can now delete items from cart

--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, Text, FlatList, Button, StyleSheet} from 'react-native';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 //import CartItem from '../../models/cart-item';
 import Colors from '../../constants/Colors';
 import CartItem from '../../components/shop/CartItem';
+import * as cartActions from '../../store/actions/cart';
 
 
 const CartScreen = props => {
@@ -21,8 +22,9 @@ const CartScreen = props => {
                 sum: state.cart.items[key].sum
             });
         }
-        return transformedCartItems;
+        return transformedCartItems.sort((a, b) => a.productId > b.productId ? 1 : -1);
     });
+    const dispatch = useDispatch();
 
     return (
             <View style={styles.screen}>
@@ -32,7 +34,9 @@ const CartScreen = props => {
                                                         quantity={itemData.item.quantity} 
                                                         title={itemData.item.productTitle}
                                                         amount={itemData.item.sum}
-                                                        onRemove={()=> {}}
+                                                        onRemove={() => {
+                                                            dispatch(cartActions.removeFromCart(itemData.item.productId));
+                                                        }}
                                                         />} 
                             />
                 <View style={styles.summary}>
