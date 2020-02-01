9f66e278f4f138b7369e82f87efb539cdaf43cbd
royce-git <54796100+royce-git@users.noreply.github.com>
2020-02-01 14:15:04 +0100

    Added user products screen and handled order screen better

--- navigation/ShopNavigator.js
+++ navigation/ShopNavigator.js
@@ -8,6 +8,7 @@ import ProductsOverviewScreen from '../screens/shop/ProductsOverviewScreen';
 import ProductDetailScreen from '../screens/shop/ProductDetailScreen';
 import CartScreen from '../screens/shop/CartScreen';
 import OrdersScreen from '../screens/shop/OrdersScreen';
+import UserProductsScreen from '../screens/user/UserProductsScreen';
 import Colors from '../constants/Colors';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -54,9 +55,24 @@ const OrdersNavigator = createStackNavigator({
     defaultNavigationOptions: defaultNavOptions
 });
 
+const AdminNavigator = createStackNavigator({
+    UserProducts: UserProductsScreen
+}, {
+    navigationOptions: {
+        drawerIcon: drawerConfig => (
+                                    <Ionicons   name={Platform.OS === 'android' ? 'md-create' : 'ios-create'}
+                                                size={23}
+                                                color={drawerConfig.tintColor}
+                                                />
+        )
+    },
+    defaultNavigationOptions: defaultNavOptions
+});
+
 const ShopNavigator = createDrawerNavigator({
     Products: ProductsNavigator,
-    Orders: OrdersNavigator
+    Orders: OrdersNavigator,
+    Admin: AdminNavigator
 }, {
     contentOptions: {
         activeTint: Colors.primary
--- screens/shop/CartScreen.js
+++ screens/shop/CartScreen.js
@@ -35,6 +35,7 @@ const CartScreen = props => {
                                                         quantity={itemData.item.quantity} 
                                                         title={itemData.item.productTitle}
                                                         amount={itemData.item.sum}
+                                                        deletable
                                                         onRemove={() => {
                                                             dispatch(cartActions.removeFromCart(itemData.item.productId));
                                                         }}
