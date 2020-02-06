cfa512ebef2a9627413f71bb05c4f73b82f0d470
royce-git <54796100+royce-git@users.noreply.github.com>
2020-02-07 00:00:01 +0100

    Connected a firebase server

--- screens/shop/ProductsOverviewScreen.js
+++ screens/shop/ProductsOverviewScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { FlatList, Button, Platform } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
@@ -7,12 +7,17 @@ import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import HeaderButton from '../../components/UI/HeaderButton';
 import ProductItem from '../../components/shop/ProductItem';
 import * as cartActions from '../../store/actions/cart';
+import * as productsActions from '../../store/actions/products';
 import Colors from '../../constants/Colors';
 
 const ProductsOverviewScreen = props => {
     const products = useSelector(state => state.products.availableProducts);
 
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(productsActions.fetchProducts())
+    }, [dispatch]);
 
     const selectItemHandler = (id, title) => {
         props.navigation.navigate('ProductDetail', { 
@@ -24,7 +29,7 @@ const ProductsOverviewScreen = props => {
     return (<FlatList 
                 data={products} 
                 keyExtractor={item => item.id} 
-                renderItem={itemData => (<ProductItem    image={itemData.item.imageUrl} 
+                renderItem={itemData => (<ProductItem   image={itemData.item.imageUrl} 
                                                         title={itemData.item.title}
                                                         price={itemData.item.price}
                                                         onSelect={() => {
